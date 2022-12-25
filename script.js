77ef1b3bd33d74b5bf7f02bfaa94b5fbea24436e
surj-mahato360 <suryamahto081@gmail.com>
2022-12-25 21:39:54 +0530

    website nearly Completed

--- script.js
+++ script.js
@@ -2,14 +2,35 @@ let menu = document.querySelector('#menu-icon');
 let Navbar = document.querySelector('.Navbar');
 let navActive = document.querySelector('.Navbar').querySelectorAll('a');
 
+$(document).ready(function() {
+    $('.Navbar li a').click(function(event) {
+        thisAttrHref = $(this).attr('href');
+        thisAttrHrefOffset = $(thisAttrHref).offset().top;
+        $('html,body').animate({scrollTop:thisAttrHrefOffset});
+        event.preventDefault();
+    });
+});
+
+$(window).scroll(function() {
+    $('.rowin').each(function() {
+        rowinTop = $(this).offset().top;
+        if($(document).scrollTop() > rowinTop){
+            activeOfs = $(this).attr('id');
+            $('.rowin').removeClass('active');
+            ActiveID = $(this).addClass('active').attr('id');
+        }
+    });
+    $('.Navbar li').each(function(index,el) {
+        thisChildren = $(this).children('a');
+        thisChildrenHref = $(this).children('a').attr('href');
+        if(thisChildrenHref === '#'+ActiveID){
+            $('.Navbar li a').removeClass('active');
+            $(thisChildren).addClass('active');
+        }
+    })
+});
+
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     Navbar.classList.toggle('open');
 }
-
-navActive.forEach(nactive => {
-    nactive.addEventListener('click',function () {
-        navActive.forEach(atv => atv.classList.remove('active'));
-        this.classList.add('active');
-    });
-});
